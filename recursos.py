8afab751da6bd7c25999681ece950ec875e70b3f
Susana <susana.garcia.dev@ejemplo.com>
2025-09-24 21:06:09 -0600

    Added post

--- recursos.py
+++ recursos.py
@@ -1,18 +1,29 @@
 from flask_restful import Resource
-from flask import render_template, make_response
+from flask import render_template, make_response, request
 
 
 class HelloWorld(Resource):
     def get(self):
-        return 'Hola mundo'
-    
+        return "Hola mundo"
+
 
 class InicioDeSesion(Resource):
     def get(self):
         return make_response(render_template("login.html"))
 
+    def post(self):
+        print("---LLA INFORMACION SE ESTA PROCESANDO")
+        correo = request.form.get("email")
+        password = request.form.get("password")
+
+        if correo == "susanlevart@gmail.com":
+            print("el correo es correcto")
+
+        print(f"Correo:{correo}")
+        print(f"Password:{password}")
+        return "procesando incion de sesion.."
+
 
 class RegistroDeUsuario(Resource):
     def get(self):
         return make_response(render_template("registro.html"))
-
